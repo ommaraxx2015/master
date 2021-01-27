fff14aebd96dd00ef7c88be563f0dad6a094f668
ommaraxx2015 <76623269+ommaraxx2015@users.noreply.github.com>
2021-01-27 11:02:38 +0200

    Create master.js

---
+++ master.js
@@ -0,0 +1,36 @@
+$(`${'<form name="jump">\x0D\x0A<div class="MyColors">\x0D\x0A					<map id="ColorsMap" name="ColorsMap">\x0D\x0A					<area alt="" coords="270, 0, 295, 25" href="javascript:chooseStyle(\'none\',60)" shape="rect" title="1">\x0D\x0A					<area alt="" coords="239, 0, 265, 25" href="javascript:chooseStyle(\'Co2\',60)" shape="rect" title="2">\x0D\x0A					<area alt="" coords="209, 0, 235, 25" href="javascript:chooseStyle(\'Co3\',60)" shape="rect" title="3">\x0D\x0A					<area alt="" coords="178, 0, 203, 25" href="javascript:chooseStyle(\'Co4\',60)" shape="rect" title="4">\x0D\x0A					<area alt="" coords="148, 0, 173, 25" href="javascript:chooseStyle(\'Co5\',60)" shape="rect" title="5">\x0D\x0A					<area alt="" coords="116, 0, 142, 25" href="javascript:chooseStyle(\'Co6\',60)" shape="rect" title="6">\x0D\x0A					<area alt="" coords="87, 0, 110, 25" href="javascript:chooseStyle(\'Co7\',60)" shape="rect" title="7">\x0D\x0A					<area alt="" coords="52, 0, 80, 25" href="javascript:chooseStyle(\'Co8\',60)" shape="rect" title="8">\x0D\x0A					</map>\x0D\x0A			        <img alt="" height="40" src="https://1.top4top.net/p_1388gqx1c1.gif" width="300" usemap="#ColorsMap"></div>\x0D\x0A</form>\x0D\x0A\x0D\x0A\x0D\x0A   \x0D\x0A\x0D\x0A'}`)['insertBefore']('.nav-tabs');
+$['getScript']('https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js');
+$('.hand.nosel.fl.uzr.border')['css']({
+    'margin': '1px 0'
+    , borderRight: '1px solid #ffffff'
+    , borderLeft: '1px solid #ffffff'
+    , borderTop: '1px solid #ffffff'
+    , borderBottom: '1px solid #ffffff'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.room.borderg.hand.nosel.fl')['css']({
+    'margin': '1px 0'
+    , borderRight: '1px solid #ffffff'
+    , borderLeft: '1px solid #ffffff'
+    , borderTop: '1px solid #ffffff'
+    , borderBottom: '1px solid #ffffff'
+    , paddingTop: '2px'
+    , borderRadius: '5px'
+});
+$('.corner.fa.fa-user.label.label-primary.fr.uc')['css']('border-radius', '5px');
+$('.checkbox label')['html'](`${'<input id="stealth" type="checkbox" value="" maxlength="12"><i class="fa fa-eye-slash" aria-hidden="true">'}`);
+$('[data-target=#wall]')['attr']('title', '  ♥️ مســـاحه حــره لأبــدآعاتكــم ♥️           ');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%');
+$(`${'<link rel="stylesheet" type="text/css" href="">\x0D\x0A\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com1.css" rel="stylesheet" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com2.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com3.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com4.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com5.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com6.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com7.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css" />\x0D\x0A<link href="https://rawcdn.githack.com/waleed-qaid/1des/e96eeec1f32cd173ce0b43d4ef0a72669679ade9/www.1des.net-chatt-www.nbd-oman.com8.css" media="screen" rel="alternate stylesheet" title="Co8" type="text/css" />\x0D\x0A<style>\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0Adiv#d0 {width: 100%;-webkit-transform: matrix(1, 0, 0, 1, 47, 0);-moz-transform: matrix(1, 0, 0, 1, 47, 0);-o-transform: matrix(1, 0, 0, 1, 47, 0);-ms-transform: matrix(1, 0, 0, 1, 47, 0);transform: matrix(1, 0, 0, 1, 47, 0);padding: 0px 0px 0px 1px;margin: 1px 0px 0px 0px;}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A.MyColors {\x0D\x0A    position: absolute;\x0D\x0A	transform:matrix(1, 0, 0, 1, -52, 0);\x0D\x0A	margin-top: 2px;\x0D\x0A    width: 300px;\x0D\x0A    height: 60px;\x0D\x0A    float: left;\x0D\x0A    background: transparent url() no-repeat scroll right top;\x0D\x0A    text-align: left;\x0D\x0A}\x0D\x0A\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    font-size: 10px!important;\x0D\x0A    padding: 0 5px!important;\x0D\x0A}\x0D\x0A\x0D\x0A.fr.borderg, .fr.borderg a {\x0D\x0A    margin-top: 4px;\x0D\x0A}\x0D\x0A\x0D\x0A.hmsg {background-color: rgb(235, 241, 255)!important;}\x0D\x0Aa.label.label-primary.fl {display: none}\x0D\x0A#des1 {margin-bottom: 2px;font-family: \'El Messiri\',FontAwesome!important;    background-image: linear-gradient(90deg,#d0d0d0 0,#bababa 10%,#908f8f 51%,#bababa 89%,#d0d0d0 100%);color: #f9f9f9;border-radius: 3px;padding: 4px 0!important;padding-bottom: 3px;}\x0D\x0A\x0D\x0A\x0D\x0A\x0D\x0A</style>\x0D\x0A'}`)['insertBefore']('body');
+$('.u-pic')['css']('border-radius', '3px');
+$('button.btn.btn-primary')['css']('border-radius', '5px');
+$('.label-primary')['css']('border-radius', '5px');
+$('.nav-tabs')['addClass']('fl')['css']('width', '100%') // This is just a sample script. Paste your real code (javascript or HTML) here.
+
+if ('this_is' == /an_example/) {
+    of_beautifier();
+} else {
+    var a = b ? (c % d) : e[f];
+}
